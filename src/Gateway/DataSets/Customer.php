e77fff1aa37ab103546e3c6ddd75f83c10bd6eba
Sergey Kibish <sk@tpro.lv>
2017-03-06 10:17:13 +0200

    Implement Customer dataset

---
+++ src/Gateway/DataSets/Customer.php
@@ -0,0 +1,187 @@
+<?php declare(strict_types = 1);
+
+/*
+ * This file is part of the transact-pro/gw3-client package.
+ *
+ * (c) Transact Pro
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TransactPro\Gateway\DataSets;
+
+use TransactPro\Gateway\Interfaces\DataSetInterface;
+
+/**
+ * Class Customer.
+ *
+ * Class Customer has all methods to fill `general-data.customer-data` block of the request.
+ */
+class Customer extends DataSet implements DataSetInterface
+{
+    /**
+     * @param  string   $email
+     * @return Customer
+     */
+    public function setEmail(string $email): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_EMAIL] = $email;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $country
+     * @return Customer
+     */
+    public function setBillingAddressCountry(string $country): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_COUNTRY] = $country;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $state
+     * @return Customer
+     */
+    public function setBillingAddressState(string $state): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_STATE] = $state;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $city
+     * @return Customer
+     */
+    public function setBillingAddressCity(string $city): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_CITY] = $city;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $street
+     * @return Customer
+     */
+    public function setBillingAddressStreet(string $street): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_BILlING_ADDRESS_STREET] = $street;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $house
+     * @return Customer
+     */
+    public function setBillingAddressHouse(string $house): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_HOUSE] = $house;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $flat
+     * @return Customer
+     */
+    public function setBillingAddressFlat(string $flat): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_FLAT] = $flat;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $zip
+     * @return Customer
+     */
+    public function setBillingAddressZIP(string $zip): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_ZIP] = $zip;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $country
+     * @return Customer
+     */
+    public function setShippingAddressCountry(string $country): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_COUNTRY] = $country;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $state
+     * @return Customer
+     */
+    public function setShippingAddressState(string $state): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_STATE] = $state;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $city
+     * @return Customer
+     */
+    public function setShippingAddressCity(string $city): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_CITY] = $city;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $street
+     * @return Customer
+     */
+    public function setShippingAddressStreet(string $street): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_STREET] = $street;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $house
+     * @return Customer
+     */
+    public function setShippingAddressHouse(string $house): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_HOUSE] = $house;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $flat
+     * @return Customer
+     */
+    public function setShippingAddressFlat(string $flat): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_FLAT] = $flat;
+
+        return $this;
+    }
+
+    /**
+     * @param  string   $zip
+     * @return Customer
+     */
+    public function setShippingAddressZIP(string $zip): self
+    {
+        $this->data[self::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_ZIP] = $zip;
+
+        return $this;
+    }
+}
---
+++ tests/Gateway/DataSets/CustomerTest.php
@@ -0,0 +1,58 @@
+<?php declare(strict_types = 1);
+
+/*
+ * This file is part of the transact-pro/gw3-client package.
+ *
+ * (c) Transact Pro
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace TransactPro\Gateway\DataSets;
+
+use PHPUnit\Framework\TestCase;
+
+class CustomerTest extends TestCase
+{
+    public function testSuccess()
+    {
+        $expected = [
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_CITY => 'a',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_COUNTRY => 'b',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_FLAT => 'c',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_HOUSE => 'd',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_STATE => 'e',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_BILlING_ADDRESS_STREET => 'f',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_BILLING_ADDRESS_ZIP => 'g',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_EMAIL => 'test@example.com',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_CITY => 'a',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_COUNTRY => 'b',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_FLAT => 'c',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_HOUSE => 'd',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_STATE => 'e',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_STREET => 'f',
+            DataSet::GENERAL_DATA_CUSTOMER_DATA_SHIPPING_ADDRESS_ZIP => 'g',
+        ];
+
+        $customer = new Customer();
+        $raw = $customer->setBillingAddressCity('a')
+            ->setBillingAddressCountry('b')
+            ->setBillingAddressFlat('c')
+            ->setBillingAddressHouse('d')
+            ->setBillingAddressState('e')
+            ->setBillingAddressStreet('f')
+            ->setBillingAddressZIP('g')
+            ->setEmail('test@example.com')
+            ->setShippingAddressCity('a')
+            ->setShippingAddressCountry('b')
+            ->setShippingAddressFlat('c')
+            ->setShippingAddressHouse('d')
+            ->setShippingAddressState('e')
+            ->setShippingAddressStreet('f')
+            ->setShippingAddressZIP('g')
+            ->getRaw();
+
+        $this->assertEquals($expected, $raw);
+    }
+}
