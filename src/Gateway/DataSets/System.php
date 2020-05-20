c72c688cfcc37ceb1071cdc10b8aaac1a8943c5e
Oleg Gorbik <oleg.gorbik@transactpro.lv>
2020-05-20 16:37:50 +0300

    Add parameters describing cardholder device

--- src/Gateway/DataSets/System.php
+++ src/Gateway/DataSets/System.php
@@ -41,4 +41,111 @@ class System extends DataSet implements DataSetInterface
 
         return $this;
     }
+
+    /**
+     * @param  string $browserAcceptHeader
+     * @return System
+     */
+    public function setBrowserAcceptHeader(string $browserAcceptHeader): self
+    {
+        $this->data[self::SYSTEM_BROWSER_ACCEPT_HEADER] = $browserAcceptHeader;
+
+        return $this;
+    }
+
+    /**
+     * @param  bool $browserJavaEnabled
+     *
+     * @return System
+     */
+    public function setBrowserJavaEnabled(bool $browserJavaEnabled): self
+    {
+        $this->data[self::SYSTEM_BROWSER_JAVA_ENABLED] = $browserJavaEnabled;
+
+        return $this;
+    }
+
+    /**
+     * @param  bool $browserJavaScriptEnabled
+     *
+     * @return System
+     */
+    public function setBrowserJavaScriptEnabled(bool $browserJavaScriptEnabled): self
+    {
+        $this->data[self::SYSTEM_BROWSER_JAVASCRIPT_ENABLED] = $browserJavaScriptEnabled;
+
+        return $this;
+    }
+
+    /**
+     * @param  string $browserLanguage
+     *
+     * @return System
+     */
+    public function setBrowserLanguage(string $browserLanguage): self
+    {
+        $this->data[self::SYSTEM_BROWSER_LANGUAGE] = $browserLanguage;
+
+        return $this;
+    }
+
+    /**
+     * @param  string $browserColorDepth
+     *
+     * @return System
+     */
+    public function setBrowserColorDepth(string $browserColorDepth): self
+    {
+        $this->data[self::SYSTEM_BROWSER_COLOR_DEPTH] = $browserColorDepth;
+
+        return $this;
+    }
+
+    /**
+     * @param  string $browserScreenHeight
+     *
+     * @return System
+     */
+    public function setBrowserScreenHeight(string $browserScreenHeight): self
+    {
+        $this->data[self::SYSTEM_BROWSER_SCREEN_HEIGHT] = $browserScreenHeight;
+
+        return $this;
+    }
+
+    /**
+     * @param  string $browserScreenWidth
+     *
+     * @return System
+     */
+    public function setBrowserScreenWidth(string $browserScreenWidth): self
+    {
+        $this->data[self::SYSTEM_BROWSER_SCREEN_WIDTH] = $browserScreenWidth;
+
+        return $this;
+    }
+
+    /**
+     * @param  string $browserTZ
+     *
+     * @return System
+     */
+    public function setBrowserTZ(string $browserTZ): self
+    {
+        $this->data[self::SYSTEM_BROWSER_TZ] = $browserTZ;
+
+        return $this;
+    }
+
+    /**
+     * @param  string $userAgent
+     *
+     * @return System
+     */
+    public function setUserAgent(string $userAgent): self
+    {
+        $this->data[self::SYSTEM_BROWSER_USER_AGENT] = $userAgent;
+
+        return $this;
+    }
 }
--- tests/Gateway/DataSets/SystemTest.php
+++ tests/Gateway/DataSets/SystemTest.php
@@ -18,14 +18,32 @@ class SystemTest extends TestCase
     public function testSuccess()
     {
         $expected = [
-            DataSet::SYSTEM_USER_IP => '127.0.0.1',
-            DataSet::SYSTEM_X_FORWARDED_FOR => '127.0.0.2',
+            DataSet::SYSTEM_USER_IP                    => '127.0.0.1',
+            DataSet::SYSTEM_X_FORWARDED_FOR            => '127.0.0.2',
+            DataSet::SYSTEM_BROWSER_ACCEPT_HEADER      => "application/json, text/javascript, */*; q=0.01",
+            DataSet::SYSTEM_BROWSER_JAVA_ENABLED       => false,
+            DataSet::SYSTEM_BROWSER_JAVASCRIPT_ENABLED => true,
+            DataSet::SYSTEM_BROWSER_LANGUAGE           => "en-US",
+            DataSet::SYSTEM_BROWSER_COLOR_DEPTH        => "24",
+            DataSet::SYSTEM_BROWSER_SCREEN_HEIGHT      => "1080",
+            DataSet::SYSTEM_BROWSER_SCREEN_WIDTH       => "1920",
+            DataSet::SYSTEM_BROWSER_TZ                 => "+300",
+            DataSet::SYSTEM_BROWSER_USER_AGENT         => "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36",
         ];
 
         $system = new System();
 
         $raw = $system->setUserIP('127.0.0.1')
             ->setXForwardedFor('127.0.0.2')
+            ->setBrowserAcceptHeader("application/json, text/javascript, */*; q=0.01")
+            ->setBrowserJavaEnabled(false)
+            ->setBrowserJavaScriptEnabled(true)
+            ->setBrowserLanguage("en-US")
+            ->setBrowserColorDepth("24")
+            ->setBrowserScreenHeight("1080")
+            ->setBrowserScreenWidth("1920")
+            ->setBrowserTZ("+300")
+            ->setUserAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36")
             ->getRaw();
 
         $this->assertEquals($expected, $raw);
